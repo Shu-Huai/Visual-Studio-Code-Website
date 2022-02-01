5032f46e449e448b75f73c636ae050695565d87c
Shu-Huai <lvzhihe_123@qq.com>
2022-02-01 23:13:22 +0800

    adjusted

--- OwiviOsa前端/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <provider>
-        <viewer />
-    </provider>
-</template>
-
-<script lang="ts" setup>
-import { Provider, Viewer } from './components'
-</script>
--- OwiviOsa前端/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Provider } from './Provider.vue'
-export { default as Viewer } from './Viewer.vue'
-export { default as PlayerCard } from './PlayerCard.vue'
